4cde7419f7125f03ac301f0ea63b00f6d842789c
Kevin Quinn <kevinquinn1918@gmail.com>
2020-03-23 18:48:53 -0400

    Added some routing

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
 import React from "react";
-import HomePage from "./pages/homepage/homepage.component";
+import { Route, Switch } from "react-router-dom";
+
 import "./App.css";
 
+import HomePage from "./pages/homepage/homepage.component";
+
+const HatsPage = () => {
+  return (
+    <div>
+      <h1>Hats Page</h1>
+    </div>
+  );
+};
+
 function App() {
-  return <HomePage></HomePage>;
+  return (
+    <div>
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route path="/hats" component={HatsPage} />
+      </Switch>
+    </div>
+  );
 }
 
 export default App;
