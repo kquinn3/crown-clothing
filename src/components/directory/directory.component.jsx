f893fe3e3e9fa57d0dc09e7068549ddeee03ab02
Kevin Quinn <kevinquinn1918@gmail.com>
2020-03-27 15:23:37 -0400

    Finished lesson 23

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -7,15 +7,13 @@ import { createStructuredSelector } from "reselect";
 import MenuItem from "../menu-item/menu-item.component";
 import "./directory.styles.scss";
 
-const Directory = ({ sections }) => {
-  return (
-    <div className="directory-menu">
-      {sections.map(({ id, ...otherSectionProps }) => (
-        <MenuItem key={id} {...otherSectionProps} />
-      ))}
-    </div>
-  );
-};
+const Directory = ({ sections }) => (
+  <div className="directory-menu">
+    {sections.map(({ id, ...otherSectionProps }) => (
+      <MenuItem key={id} {...otherSectionProps} />
+    ))}
+  </div>
+);
 
 const mapStateToProps = createStructuredSelector({
   sections: SelectDirectorySections
